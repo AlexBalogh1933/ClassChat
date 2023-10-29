8752f23edb44d76099e6108fb9e366332512c66e
colton <daltonco@mail.uc.edu>
2023-10-29 17:15:21 -0400

    implemented 2toad profanity filter
    added dependencies and implemented filter on 'silent' command.

--- src/silentCommandHandler.js
+++ src/silentCommandHandler.js
@@ -1,26 +1,44 @@
 const silentCard = require("./adaptiveCards/silentCommand.json");
 const { AdaptiveCards } = require("@microsoft/adaptivecards-tools");
 const { CardFactory, MessageFactory } = require("botbuilder");
+var profanity = require('@2toad/profanity').profanity;
 
 class SilentCommandHandler {
   triggerPatterns = "silent";
 
   async handleCommandReceived(context, message){
+    // trim 'silent' from the message
+    var sentMessage = trimMessage(message);
+
     //verify the command arguments received from the client
-    console.log(`Silenced Message: ${message.text}`);
+    console.log(`Silenced Message: ${sentMessage}`);
 
-    const cardData = {
-      title: "Silent command is added!",
-      body: "Congratulations! You have responded to silent command",
-    };
+    // run the message through the @2toad profanity filter
+    if(profanity.exists(sentMessage)){
+      console.log(`Message contained probhibited text, request cancelled.`);
+    }
+    else{
+      // render the silentCommand adaptive card
+      const cardData = {
+        title: "A student asked:",
+        body: sentMessage,
+      };
 
-    const cardJson = AdaptiveCards.declare(silentCard).render(cardData);
-    return MessageFactory.attachment(CardFactory.adaptiveCard(cardJson));
+      const cardJson = AdaptiveCards.declare(silentCard).render(cardData);
+      return MessageFactory.attachment(CardFactory.adaptiveCard(cardJson));
+    }
+  
   }
 }
 
-//Methods
+// Methods
 
+// Removes the 'silent' command from the message
+function trimMessage(message){
+  return message.text.slice(6);
+}
+
+// Exports
 module.exports = {
   SilentCommandHandler,
 }
